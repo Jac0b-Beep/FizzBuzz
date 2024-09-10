dafcb58da0f70b6463e658b170a71b8e5d9d363b
Jacob <jacob.lisogurski@mail.utoronto.ca>
2024-09-10 15:45:43 -0400

    new file to count how many multiples of 3 or 5 less than a given number

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+public class Multiples
+{
+    public static void main(String[] args)
+    {
+        System.out.println(multiplesOfThreeOrFive(1000));
+    }
 
-public class Multiples {
+    public static int multiplesOfThreeOrFive(int max)
+    {
+        int count = 0;
+
+        for (int i = 1; i < max; i++)
+        {
+            if (i % 3 == 0 || i % 5 == 0)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
